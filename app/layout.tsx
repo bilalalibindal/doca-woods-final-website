dfa8721d399edc85f65e6498948974aa174eb8bb
bilalalibindal <bilalalibindal@gmail.com>
2025-09-12 10:10:33 +0300

    Toaster position düzenlendi

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,15 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>{children}</AuthProvider>
-        <Toaster position="top-right" expand={true} richColors closeButton />
+        <Toaster
+          toastOptions={{
+            duration: 2000,
+          }}
+          position="bottom-right"
+          expand={true}
+          richColors
+          closeButton
+        />
       </body>
     </html>
   );
